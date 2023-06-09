b49ca4d0b476e3899a09608a2a9e29f4ceb505a1
Kauã Maximino <kaua_maximino@outlook.com>
2023-06-09 14:36:07 -0300

    refactor: :recycle: fix spaces

--- mongo/baixaEstoque.mongodb.js
+++ mongo/baixaEstoque.mongodb.js
@@ -2,7 +2,7 @@ use('ecomm')
 
 db.products.updateOne(
   { NOME: "Galaxy Tab S8" },
-  {"QUANTIDADE EM ESTOQUE": {$gte: 2}},
+  { "QUANTIDADE EM ESTOQUE": {$gte: 2} },
   { $inc: { "QUANTIDADE EM ESTOQUE": -2 } }
 );
 
