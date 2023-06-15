d937d2a69670fddebf417e787ce0fc29edbf33f6
Kauã Maximino <kaua_maximino@outlook.com>
2023-06-15 18:25:14 -0300

    feat: :sparkles: new methods for create, update and delete categories

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -13,6 +13,24 @@ class CategoryService {
     console.log(`response status: ${chalk.yellow(response.status)}`)
     return response.data;
   }
+
+  static async createCategory(category) {
+    const response = await axios.post('http://127.0.0.1:3000/categories', category);
+    console.log(`response status: ${chalk.yellow(response.status)}`)
+    return response.data;
+  }
+
+  static async updateCategory(id, category) {
+    const response = await axios.put(`http://127.0.0.1:3000/categories/${id}`, category);
+    console.log(`response status: ${chalk.yellow(response.status)}`)
+    return response.data;
+  }
+
+  static async deleteCategory(id) {
+    const response = await axios.delete(`http://127.0.0.1:3000/categories/${id}`);
+    console.log(`response status: ${chalk.yellow(response.status)}`)
+    return response.data;
+  }
 }
 
 export default CategoryService;
