43dea32d61ab402f929fa7236de32d27cf157a2e
Kauã Maximino <kaua_maximino@outlook.com>
2023-06-22 13:49:57 -0300

    feat: :sparkles: CRUD in /categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -4,9 +4,9 @@ class CategoryController {
   static async getAllCategories(request, response) {
     try {
       const categories = await CategoryModel.find();
-      response.status(200).json(categories);
+      return response.status(200).json(categories);
     } catch (error) {
-      response.status(500).json({ message: error.message });
+      return response.status(500).json({ message: error.message });
     }
   }
 
@@ -15,12 +15,11 @@ class CategoryController {
       const { id } = request.params;
       const category = await CategoryModel.findById(id);
       if (!category) {
-        response.status(404).json({ message: 'Category not found' });
-      } else {
-        response.status(200).json(category);
+        return response.status(404).json({ message: 'Category not found' });
       }
+      return response.status(200).json(category);
     } catch (error) {
-      response.status(500).json({ message: error.message });
+      return response.status(500).json({ message: error.message });
     }
   }
 
@@ -28,9 +27,36 @@ class CategoryController {
     try {
       const category = new CategoryModel(request.body);
       await category.save();
-      response.status(201).json(category);
+      return response.status(201).json(category);
     } catch (error) {
-      response.status(500).json({ message: error.message });
+      return response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async updateCategory(request, response) {
+    try {
+      const { id } = request.params;
+      const category = await CategoryModel.findById(id);
+      if (!category) {
+        return response.status(404).json({ message: 'Category not found' });
+      }
+      await CategoryModel.updateOne({ _id: id }, request.body);
+      return response.status(200).json({ _id: id, ...request.body });
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async deleteCategory(request, response) {
+    try {
+      const { id } = request.params;
+      const categoryDeleted = await CategoryModel.findByIdAndDelete(id);
+      if (!categoryDeleted) {
+        return response.status(404).json({ message: 'Category not found' });
+      }
+      return response.status(200).json({ message: 'Category deleted' });
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
     }
   }
 }
