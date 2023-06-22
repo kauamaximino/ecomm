f1c18b8e21eb3226d2d0ec2f4d7ea90adaa6bc15
Kauã Maximino <kaua_maximino@outlook.com>
2023-06-22 12:07:18 -0300

    test: :test_tube: test for /categories

---
+++ src/test/routes/categories.test.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+import request from 'supertest';
+import {
+  describe, expect, it,
+} from '@jest/globals';
+
+describe('GET em /categories', () => {
+  it('should return all categories', async () => {
+    const response = await request('http://localhost:3000').get('/categories');
+    expect(response.status).toBe(200);
+
+    response.body.forEach((category) => {
+      expect(category).toHaveProperty('_id');
+      expect(category).toHaveProperty('nome');
+      expect(category).toHaveProperty('status');
+    });
+  });
+
+  it('should return a category by id', async () => {
+    const response = await request('http://localhost:3000')
+      .get('/categories');
+    const { _id, nome, status } = response.body[0];
+
+    const responseId = await request('http://localhost:3000')
+      .get(`/categories/${_id}`);
+    expect(responseId.status).toBe(200);
+    expect(responseId.body).toHaveProperty('_id');
+    expect(responseId.body.nome).toBe(nome);
+    expect(responseId.body.status).toBe(status);
+  });
+});
+
+describe('POST em /categories', () => {
+  it('should insert a category', async () => {
+    const category = {
+      nome: 'Teste',
+      status: 'ATIVO',
+    };
+
+    const response = await request('http://localhost:3000')
+      .post('/categories')
+      .send(category);
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('_id');
+    expect(response.body.nome).toBe(category.nome);
+    expect(response.body.status).toBe(category.status);
+  });
+
+  it(
+    'should return 500 when insert a category with invalid data',
+    async () => {
+      const category = {
+        nome: '',
+        status: 'ATIVO',
+      };
+
+      const response = await request('http://localhost:3000')
+        .post('/categories')
+        .send(category);
+      expect(response.status).toBe(500);
+      expect(response.body).toHaveProperty('message');
+    },
+  );
+});
